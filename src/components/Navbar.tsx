08cff6c74da14c02832064e75a354063effa8d9f
niklusrook <post@nikolas.no>
2024-04-22 12:30:55 +0200

    update: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,13 @@
 
 
 //gaymærn skriver her din stygge ape
+
+const Navbar = () => {
+    return(
+        <div className='sticky inset-x-0 top-0 z-30 w-full transition-all border-b border-gray-200 bg-white/75 backdrop-blur-lg'>
+            <h1>navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
