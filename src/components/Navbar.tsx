f38de6208a9ca89b88f3f7bf4e2c7ed7d3158316
niklusrook <post@nikolas.no>
2024-04-26 10:11:51 +0200

    update: title and description in layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,15 +71,11 @@ export default function Navbar() {
                       <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                         <li className="row-span-3">
                           <NavigationMenuLink asChild>
-                            <a
-                              className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                              href="/"
-                            >
-                              <AvatarIcon className="h-6 w-6" />
-                              <div className="mb-2 mt-4 text-lg font-medium">
-                                  Ringsaker                            
-                              </div>
-                            </a>
+                              <img
+                                src="/land-ringsaker.png"
+                                alt="ringsaker"
+                                className="h-full w-full rounded-md"
+                              />
                           </NavigationMenuLink>
                         </li>
                         <ListItem href="https://www.ringsaker.kommune.no" title="Om Selskapet">
