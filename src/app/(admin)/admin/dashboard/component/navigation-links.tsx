377f487c710c8204961606f041362a9e5d956393
niklusrook <post@nikolas.no>
2024-05-02 13:28:29 +0200

    fix: typo

--- src/app/(admin)/admin/dashboard/component/navigation-links.tsx
+++ src/app/(admin)/admin/dashboard/component/navigation-links.tsx
@@ -14,17 +14,18 @@ const Navigationlink = () => {
     return (
         <div className="space-x-4">
             {links.map((link, index) => (
-                <Link 
-                    key={index} // Added key prop here
-                    className={cn({
-                        "bg-red-500 text-primary-foreground": link.href === currentPath,
-                        "border border-input bg-background hover:bg-accent": link.href!== currentPath,
-                        "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2": true,
-                    })}
-                    href={link.href}
-                >
-                    {link.label}
-                </Link>
+                <div key={index}>
+                    <Link 
+                        className={cn({
+                            "bg-red-500 text-primary-foreground": link.href === currentPath,
+                            "border border-input bg-background hover:bg-accent": link.href!== currentPath,
+                            "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2": true,
+                        })}
+                        href={link.href}
+                    >
+                        {link.label}
+                    </Link>
+                </div>
             ))}
         </div>
     );
