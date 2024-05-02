bf17485c425c23f0d9fe3a5114441ceeb8644bb8
Nikolas-Vardeberg <post@nikolas.no>
2024-05-02 22:25:03 +0200

    fix typo

--- src/app/(admin)/admin/dashboard/Header.tsx
+++ src/app/(admin)/admin/dashboard/Header.tsx
@@ -3,7 +3,6 @@ import { redirect, usePathname } from "next/navigation";
 import { Button } from "../../../../components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import Navigationlink from "@/app/(admin)/component/navigation-links";
 
 
 
